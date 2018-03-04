3e2ac19843a5879834792773e20c482b01f58131
Vyacheslav Enis <venis@difane.com>
2018-03-05 00:02:03 +0200

    Initial Commit

---
+++ DesktopOSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DesktopOSX
+//
+//  Created by VIACHESLAV ENIS on 3/5/18.
+//  Copyright © 2018 Another Memory. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DesktopOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DesktopOSX
+//
+//  Created by VIACHESLAV ENIS on 3/5/18.
+//  Copyright © 2018 Another Memory. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
