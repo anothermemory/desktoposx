f74e133376372792c5a763ee7ba909fd0a5973f3
Vyacheslav Enis <venis@difane.com>
2018-03-05 10:18:44 +0200

    Initial test configuration with some dummy spec

---
+++ DesktopOSX/MyDummyEnum.swift
@@ -0,0 +1,11 @@
+//
+// Created by VIACHESLAV ENIS on 3/5/18.
+// Copyright (c) 2018 Another Memory. All rights reserved.
+//
+
+import Foundation
+
+enum MyDummyEnum {
+    case First
+    case Second
+}
---
+++ DesktopOSXTests/MyDummyEnumTests.swift
@@ -0,0 +1,26 @@
+//
+// Created by VIACHESLAV ENIS on 3/5/18.
+// Copyright (c) 2018 Another Memory. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import DesktopOSX
+
+class MyDummyEnumSpec: QuickSpec {
+    override func spec() {
+        it("is simple") {
+            expect(MyDummyEnum.First).to(equal(MyDummyEnum.First))
+        }
+
+        describe("MyDummyEnum") {
+            describe("its First") {
+                it("is easy to use") {
+                    expect(MyDummyEnum.First).to(equal(MyDummyEnum.First))
+                    expect(MyDummyEnum.First).toNot(equal(MyDummyEnum.Second))
+                }
+            }
+        }
+    }
+}
