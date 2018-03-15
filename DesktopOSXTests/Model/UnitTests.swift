b7c9a364f742a1b808b3a5575f403554ab4ab6de
Vyacheslav Enis <venis@difane.com>
2018-03-15 20:50:46 +0200

    Added test for text plain

--- DesktopOSXTests/Model/UnitTests.swift
+++ DesktopOSXTests/Model/UnitTests.swift
@@ -8,6 +8,30 @@ import Nimble
 
 @testable import DesktopOSX
 
+let unitExample = "unit"
+
+class UnitSharedExamplesConfiguration: QuickConfiguration {
+    override class func configure(_ configuration: Configuration!) {
+        sharedExamples(unitExample) { (sharedExampleContext: @escaping SharedExampleContext) in
+            it("has common unit properties") {
+                let unit: AMUnit! = sharedExampleContext()["unit"] as? AMUnit
+
+                unit.uid = "789"
+                unit.title = "Updated"
+                let newCreatedDate = Date(timeIntervalSince1970: TimeInterval(3))
+                unit.created = newCreatedDate
+                let newUpdatedDate = Date(timeIntervalSince1970: TimeInterval(4))
+                unit.updated = newUpdatedDate
+
+                expect(unit.uid) == "789"
+                expect(unit.title) == "Updated"
+                expect(unit.created) == newCreatedDate
+                expect(unit.updated) == newUpdatedDate
+            }
+        }
+    }
+}
+
 class UnitSpec: QuickSpec {
     override func spec() {
         describe("a unit") {
@@ -24,18 +48,34 @@ class UnitSpec: QuickSpec {
                 expect(unit.type).to(equal(AMUnitType.unit))
             }
 
-            it("can have properties changed") {
-                unit.uid = "789"
-                unit.title = "Updated"
-                let newCreatedDate = Date(timeIntervalSince1970: TimeInterval(3))
-                unit.created = newCreatedDate
-                let newUpdatedDate = Date(timeIntervalSince1970: TimeInterval(4))
-                unit.updated = newUpdatedDate
+            itBehavesLike(unitExample) {
+                ["unit": unit]
+            }
+        }
 
-                expect(unit.uid) == "789"
-                expect(unit.title) == "Updated"
-                expect(unit.created) == newCreatedDate
-                expect(unit.updated) == newUpdatedDate
+        describe("a plain text unit") {
+            var unit: AMUnitTextPlain!
+            beforeEach {
+                unit = AMUnitTextPlain(
+                        uid: "123",
+                        title: "MyUnit",
+                        created: Date(timeIntervalSince1970: TimeInterval(1)),
+                        updated: Date(timeIntervalSince1970: TimeInterval(2)),
+                        data: "data"
+                        )
+            }
+
+            it("has type 'textPlain'") {
+                expect(unit.type).to(equal(AMUnitType.textPlain))
+            }
+
+            it("can store textual data") {
+                unit.data = "test"
+                expect(unit.data) == "test"
+            }
+
+            itBehavesLike(unitExample) {
+                ["unit": unit]
             }
         }
     }
