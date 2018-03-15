aac23856e487a53e4b5ef28a5dd8a6b8e85baab8
Vyacheslav Enis <venis@difane.com>
2018-03-15 20:56:53 +0200

    Added more tests for markdown/language

--- DesktopOSXTests/Model/UnitTests.swift
+++ DesktopOSXTests/Model/UnitTests.swift
@@ -9,6 +9,7 @@ import Nimble
 @testable import DesktopOSX
 
 let unitExample = "unit"
+let unitTextPlainExample = "unitTextPlain"
 
 class UnitSharedExamplesConfiguration: QuickConfiguration {
     override class func configure(_ configuration: Configuration!) {
@@ -27,6 +28,15 @@ class UnitSharedExamplesConfiguration: QuickConfiguration {
                 expect(unit.title) == "Updated"
                 expect(unit.created) == newCreatedDate
                 expect(unit.updated) == newUpdatedDate
+            }
+        }
+
+        sharedExamples(unitTextPlainExample) { (sharedExampleContext: @escaping SharedExampleContext) in
+            it("can store textual data") {
+                let unit: AMUnitTextPlain! = sharedExampleContext()["unit"] as? AMUnitTextPlain
+
+                unit.data = "test"
+                expect(unit.data) == "test"
             }
         }
     }
@@ -69,14 +79,54 @@ class UnitSpec: QuickSpec {
                 expect(unit.type).to(equal(AMUnitType.textPlain))
             }
 
-            it("can store textual data") {
-                unit.data = "test"
-                expect(unit.data) == "test"
+            itBehavesLike(unitExample) {["unit": unit]}
+            itBehavesLike(unitTextPlainExample) {["unit": unit]}
+        }
+
+        describe("a markdown text unit") {
+            var unit: AMUnitTextMarkdown!
+            beforeEach {
+                unit = AMUnitTextMarkdown(
+                        uid: "123",
+                        title: "MyUnit",
+                        created: Date(timeIntervalSince1970: TimeInterval(1)),
+                        updated: Date(timeIntervalSince1970: TimeInterval(2)),
+                        data: "data"
+                        )
             }
 
-            itBehavesLike(unitExample) {
-                ["unit": unit]
+            it("has type 'textMarkdown'") {
+                expect(unit.type).to(equal(AMUnitType.textMarkdown))
             }
+
+            itBehavesLike(unitExample) {["unit": unit]}
+            itBehavesLike(unitTextPlainExample) {["unit": unit]}
+        }
+
+        describe("a code text unit") {
+            var unit: AMUnitTextCode!
+            beforeEach {
+                unit = AMUnitTextCode(
+                        uid: "123",
+                        title: "MyUnit",
+                        created: Date(timeIntervalSince1970: TimeInterval(1)),
+                        updated: Date(timeIntervalSince1970: TimeInterval(2)),
+                        data: "data",
+                        language: "PHP"
+                        )
+            }
+
+            it("has type 'textCode'") {
+                expect(unit.type).to(equal(AMUnitType.textCode))
+            }
+
+            it("can store language") {
+                unit.language = "Java"
+                expect(unit.language) == "Java"
+            }
+
+            itBehavesLike(unitExample) {["unit": unit]}
+            itBehavesLike(unitTextPlainExample) {["unit": unit]}
         }
     }
 }
